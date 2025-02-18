56180e7409236500cb65f9dcd7d381391f683f87
Frandy R. Nuñez <ydnarf13@hotmail.com>
2025-02-18 16:02:58 -0400

    websocket creado

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const Server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+    console.log('A user connected')
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
