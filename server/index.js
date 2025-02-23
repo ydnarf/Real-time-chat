38edd61ad98766c059aaa8dba552d6603b29e335
Frandy R. Nuñez <ydnarf13@hotmail.com>
2025-02-22 21:18:02 -0400

    Comunicacion cliente con el servidor creada

--- server/index.js
+++ server/index.js
@@ -11,20 +11,24 @@ const server = createServer(app)
 const io = new Server(server)
 
 io.on('connection', (socket) => {
-    console.log('A user connected')
+  console.log('A user connected')
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected')
-    })
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log(`message: ` + msg)
+  })
 })
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
 
