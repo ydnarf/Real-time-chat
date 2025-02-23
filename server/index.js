3b674a76da4ad87b4846d4ecea0762ed09372298
Frandy R. Nuñez <ydnarf13@hotmail.com>
2025-02-23 12:36:18 -0400

    Recuperar los mensajes anteriores

--- server/index.js
+++ server/index.js
@@ -21,30 +21,21 @@ const db = createClient({
   authToken: process.env.DB_TOKEN
 })
 
-async function initializeDatabase() {
-  try {
-    await db.execute(`
+
+await db.execute(`
       CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         Content TEXT
       )
     `)
-    console.log("Database initialized.")
-  } catch (e) {
-    console.error("Error initializing database:", e)
-  }
-}
 
 
-initializeDatabase()
-
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('A user connected')
 
   socket.on('disconnect', () => {
     console.log('User disconnected')
   })
-
 
   socket.on('chat message', async (msg) => {
     let result
@@ -56,8 +47,27 @@ io.on('connection', (socket) => {
       io.emit('chat message', msg, result.lastInsertRowid.toString())
     } catch (e) {
       console.error("Error inserting message:", e)
+      return
     }
   })
+
+  console.log('auth')
+  console.log(socket.handshake.auth)
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: `SELECT id, Content FROM messages WHERE id > ?`,
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.Content, row.id.toString())
+      })
+    } catch (e) {
+      console.error("Error fetching messages:", e)
+    }
+  }
 })
 
 app.use(logger('dev'))
